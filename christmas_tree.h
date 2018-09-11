20ee164da4a12d743db6d0e884404015aa929982
zhangxinyao88 <43081360+zhangxinyao88@users.noreply.github.com>
2018-09-10 23:19:59 -0400

    Update and rename christmas tree to christmas_tree.h

--- christmas_tree.h
+++ christmas_tree.h
@@ -32,12 +32,4 @@ double f(double px, double py, double theta, double scale, int n) {
     return d;
 }
 
-void main()
- {
-	char ch;
-    for (sy = 0.8f; sy > 0.0f; sy -= 0.02f,cout<<'\n')  
-        for (sx = -0.35f; sx < 0.35f; sx += 0.01f)
-		{//ch=f(0, 0, PI * 0.5f, 1.0f, n) < 0 ? '*' : ' ';
-			ch=f(0, 0, PI * 0.5f, 1.0f, 3) < 0 ? '*' : ' ';
-		cout<<ch;}
-}
+
